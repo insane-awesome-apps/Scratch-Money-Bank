7ced10112be9c20ae111ffb910ae25d9978bf705
insane-awesome-apps <awesome_apps@outlook.com>
2024-11-16 21:18:46 +0200

    deleted the test file, "test.py"

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Access the secret as an environment variable
-my_secret = os.environ.get("MY_SECRET")
-
-if my_secret:
-    print("Successfully retrieved the secret!")
-else:
-    print("Secret MY_SECRET is not set.")
